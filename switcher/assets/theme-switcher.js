8370d4043b4861ff0950a0041d8bb39cedd7dbfc
Max <12maks32@gmail.com>
2023-06-12 20:25:27 +0200

    Added comments to script

--- switcher/assets/theme-switcher.js
+++ switcher/assets/theme-switcher.js
@@ -1,68 +1,86 @@
-const htmlBlock = document.documentElement
+// Get a reference to the root HTML element
+const htmlBlock = document.documentElement;
 
-const saveUserTheme = localStorage.getItem('user-theme')
+// Retrieve the user's saved theme from local storage
+const saveUserTheme = localStorage.getItem('user-theme');
 
-let resetButton
+let resetButton; // Variable to store reset button(s)
 
-let userTheme
+let userTheme; // Variable to store the user's preferred theme based on their system preferences
 if (window.matchMedia) {
-    userTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark-theme' : 'light-theme'
+    // Check if the browser supports the matchMedia API
+    // Set userTheme to 'dark-theme' if the system prefers dark color scheme, otherwise set it to 'light-theme'
+    userTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark-theme' : 'light-theme';
 }
+// Listen for changes in the system's preferred color scheme
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-    !saveUserTheme ? changeTheme() : null
-})
+    // If no user theme is saved, change the theme
+    !saveUserTheme ? changeTheme() : null;
+});
 
+// Function to set the theme class on the root HTML element
 function setThemeClass() {
     if (saveUserTheme) {
-        htmlBlock.classList.add(saveUserTheme)
+        // If a user theme is saved, add it as a class to the root HTML element
+        htmlBlock.classList.add(saveUserTheme);
         if (resetButton != null) {
+            // If resetButton is defined, add the 'active' class to all reset buttons
             resetButton.forEach(button => {
-                button.classList.add('active')
-            })
+                button.classList.add('active');
+            });
         }
     } else {
-        htmlBlock.classList.add(userTheme)
+        // If no user theme is saved, add the user's preferred theme as a class to the root HTML element
+        htmlBlock.classList.add(userTheme);
     }
 }
 
-setThemeClass()
+setThemeClass(); // Call the setThemeClass function to set the initial theme
 
+// Function to change the theme
 function changeTheme(saveTheme = false) {
-    let currentTheme = htmlBlock.classList.contains('light-theme') ? 'light-theme' : 'dark-theme'
-    let newTheme
+    let currentTheme = htmlBlock.classList.contains('light-theme') ? 'light-theme' : 'dark-theme';
+    let newTheme;
 
     if (currentTheme === 'light-theme') {
-        newTheme = 'dark-theme'
+        newTheme = 'dark-theme';
     } else if (currentTheme === 'dark-theme') {
-        newTheme = 'light-theme'
+        newTheme = 'light-theme';
     }
-    htmlBlock.classList.remove(currentTheme)
-    htmlBlock.classList.add(newTheme)
-    saveTheme ? localStorage.setItem('user-theme', newTheme) : null
+    // Remove the current theme class and add the new theme class to the root HTML element
+    htmlBlock.classList.remove(currentTheme);
+    htmlBlock.classList.add(newTheme);
+    // If saveTheme is true, save the new theme in local storage
+    saveTheme ? localStorage.setItem('user-theme', newTheme) : null;
 }
 
+// Event listener that executes when the window finishes loading
 window.addEventListener('load', () => {
-
-    const themeButton = document.querySelectorAll('.switch-theme__button')
-    resetButton = document.querySelectorAll('.switch-theme__reset')
+    // Get references to theme buttons and reset buttons
+    const themeButton = document.querySelectorAll('.switch-theme__button');
+    resetButton = document.querySelectorAll('.switch-theme__reset');
     if (themeButton) {
+        // If theme buttons exist, add event listeners to each button
         themeButton.forEach(button => {
             button.addEventListener('click', function(event) {
                 if (resetButton) {
+                    // If reset buttons exist, add the 'active' class to all reset buttons
                     resetButton.forEach(button => {
-                        button.classList.add('active')
-                    })
+                        button.classList.add('active');
+                    });
                 }
-                changeTheme(true)
-            })
-        })
+                changeTheme(true); // Call the changeTheme function and save the theme
+            });
+        });
     }
     if (resetButton) {
+        // If reset buttons exist, add event listeners to each button
         resetButton.forEach(button => {
             button.addEventListener('click', function(event) {
-                resetButton.classList.remove('active')
-                localStorage.setItem('user-theme', '')
-            })
-        })
+                resetButton.classList.remove('active');
+                // Remove the user theme from local storage
+                localStorage.setItem('user-theme', '');
+            });
+        });
     }
 })
